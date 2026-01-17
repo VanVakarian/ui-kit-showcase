5bd58e277d3b241cdd8737276bf25cc6412e3968
VanVakarian <void@vslav.dev>
2026-01-17 12:07:20 +0400

    ✨ Added date range slider with custom formatting

--- src/app/components/ui-showcase/pages/v-slider/v-slider.ts
+++ src/app/components/ui-showcase/pages/v-slider/v-slider.ts
@@ -13,4 +13,25 @@ export class VSliderPage {
   protected readonly singleValue$$ = signal(35);
   protected readonly rangeValue$$ = signal<[number, number]>([20, 80]);
   protected readonly rangeNegativeValue$$ = signal<[number, number]>([-50, 50]);
+  protected readonly dateList = [
+    new Date(2025, 0, 5),
+    new Date(2025, 0, 12),
+    new Date(2025, 0, 19),
+    new Date(2025, 0, 26),
+    new Date(2025, 1, 2),
+    new Date(2025, 1, 9),
+    new Date(2025, 1, 16),
+    new Date(2025, 1, 23),
+  ];
+  protected readonly dateValues = this.dateList.map((value) => value.getTime());
+  protected readonly dateRange$$ = signal<[number, number]>([this.dateValues[1], this.dateValues[6]]);
+  protected readonly dateFormatter = new Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  });
+
+  protected formatDate(value: number): string {
+    return this.dateFormatter.format(new Date(value));
+  }
 }
