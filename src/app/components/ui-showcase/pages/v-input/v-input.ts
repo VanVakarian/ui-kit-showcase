bf3d041df5ef6bc907413e7aef823394dfadce98
VanVakarian <void@vslav.dev>
2026-01-20 13:54:56 +0400

    ✨ Added auto submit feature to v-input component

--- src/app/components/ui-showcase/pages/v-input/v-input.ts
+++ src/app/components/ui-showcase/pages/v-input/v-input.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { VButton } from '@ui-kit/components/v-button/v-button';
 import { IconName, VIcon } from '@ui-kit/components/v-icon/v-icon';
-import { VInput } from '@ui-kit/components/v-input/v-input';
+import { VInput, VInputAutoSubmitResult } from '@ui-kit/components/v-input/v-input';
 
 @Component({
   selector: 'v-input-page',
@@ -12,6 +12,11 @@ import { VInput } from '@ui-kit/components/v-input/v-input';
 })
 export class VInputPage {
   protected readonly Icon = IconName;
+  protected readonly autoSubmitResult$$ = signal<VInputAutoSubmitResult | null>(null);
+  protected readonly autoSubmitStatusText$$ = signal('');
+
+  private autoSubmitResponseTimeoutId: ReturnType<typeof setTimeout> | null = null;
+  private autoSubmitResetTimeoutId: ReturnType<typeof setTimeout> | null = null;
 
   protected readonly form = new FormGroup({
     testInput: new FormControl(''),
@@ -24,5 +29,34 @@ export class VInputPage {
 
   protected consoleLogFormValue(): void {
     console.log(this.form.value);
+  }
+
+  protected handleAutoSubmit(value: string | number | null): void {
+    if (this.autoSubmitResponseTimeoutId) {
+      clearTimeout(this.autoSubmitResponseTimeoutId);
+    }
+
+    if (this.autoSubmitResetTimeoutId) {
+      clearTimeout(this.autoSubmitResetTimeoutId);
+    }
+
+    this.autoSubmitResult$$.set(null);
+    this.autoSubmitStatusText$$.set('Submitting...');
+
+    const minResponseDelayMs = 500;
+    const maxResponseDelayMs = 2000;
+    const responseDelayRangeMs = maxResponseDelayMs - minResponseDelayMs + 1;
+    const responseDelay = Math.floor(Math.random() * responseDelayRangeMs) + minResponseDelayMs;
+
+    this.autoSubmitResponseTimeoutId = setTimeout(() => {
+      const isSuccess = Math.random() < 0.5;
+
+      this.autoSubmitResult$$.set(isSuccess ? VInputAutoSubmitResult.Success : VInputAutoSubmitResult.Error);
+      this.autoSubmitStatusText$$.set(isSuccess ? 'Auto submit succeeded' : 'Auto submit failed');
+
+      this.autoSubmitResetTimeoutId = setTimeout(() => {
+        this.autoSubmitResult$$.set(null);
+      }, 3000);
+    }, responseDelay);
   }
 }
