34db4df206067b1e812fa253881eb2b4e080f8cd
VanVakarian <void@vslav.dev>
2026-01-22 16:33:15 +0400

    🔨 More v-button page improvements

--- src/app/components/ui-showcase/pages/v-button/v-button.ts
+++ src/app/components/ui-showcase/pages/v-button/v-button.ts
@@ -1,3 +1,4 @@
+import { NgTemplateOutlet } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { VButton } from '@ui-kit/components/v-button/v-button';
 import { IconName, VIcon } from '@ui-kit/components/v-icon/v-icon';
@@ -6,8 +7,22 @@ import { IconName, VIcon } from '@ui-kit/components/v-icon/v-icon';
   selector: 'v-button-page',
   templateUrl: './v-button.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [VButton, VIcon],
+  imports: [NgTemplateOutlet, VButton, VIcon],
 })
 export class VButtonPage {
   protected readonly Icon = IconName;
+
+  protected readonly buttonStyles = [
+    { class: 'v-link', label: '.v-link' },
+    { class: 'v-flat', label: '.v-flat' },
+    { class: 'v-raised', label: '.v-raised' },
+  ];
+
+  protected readonly buttonRoles = [
+    { class: '', label: '-' },
+    { class: 'v-primary', label: '.v-primary' },
+    { class: 'v-accent', label: '.v-accent' },
+    { class: 'v-danger', label: '.v-danger' },
+    { class: 'v-primary v-hover', label: '.v-primary .v-hover' },
+  ];
 }
